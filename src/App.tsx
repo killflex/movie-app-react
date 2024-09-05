69c909a60400bbc39e4e16cac792f32818cb4dcf
killflex <ferryhasanbackup@gmail.com>
2024-09-05 22:21:08 +0700

    feat(navbar): Add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="">App</div>;
+  return (
+    <div className="">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/movies" element={<h1>Movies Page</h1>} />
+          <Route
+            path="/moviedetails/:id"
+            element={<h1>Home Details Page</h1>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#121212] py-3">
+      <div className="flex flex-row justify-between items-center w-[80%] mx-auto ">
+        <div className="flex flex-row space-x-16">
+          <div className="flex flex-col text-yellow-500">
+            <h1 className="text-lg leading-4">ALLABOUT</h1>
+            <h1 className="text-2xl leading-5 font-semibold">MOVIES</h1>
+          </div>
+          <button className="text-lg text-yellow-500 hover:underline">
+            EXPLORE
+          </button>
+        </div>
+        <div className="">
+          <input
+            placeholder="Search..."
+            className="w-[300px] h-[35px] outline-none px-2 rounded-md bg-black text-[#c2c2c2] text-lg placeholder:text-[#646464]"
+            type="text"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
