6aae32457d5bd71bae106cc0ceb0417991789ce6
killflex <ferryhasanbackup@gmail.com>
2024-09-06 04:59:50 +0700

    feat(404): Add route to 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Movies from "./pages/Movies/Movies";
 import Details from "./pages/Details/Details";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/moviedetails/:id" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
